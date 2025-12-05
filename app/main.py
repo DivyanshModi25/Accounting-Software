9e5a8e79d921647754aaeb97e7a24ebd9ed815ec
DivyanshModi25 <divyanshmodi2510@gmail.com>
2025-12-05 23:23:43 +0530

    adding the main file boiler code

--- app/main.py
+++ app/main.py
@@ -1,10 +1,45 @@
-from core.config import settings
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
-def main():
-    print("Hello from accounting-software!")
-    print(settings.database_url)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup logic 
+    # await init_db
+
+    yield
+
+    # shutdown logic
+    # await close_db()
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(
+    title="Accounting software API",
+    version="1.0.0",
+    lifespan=lifespan,  # register lifespan handler 
+)
+
+
+# origins = [
+#     "http://localhost:3000",  # e.g. React frontend
+#     "http://127.0.0.1:3000",
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,          # or ["*"] during development
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+# app.include_router(api_v1_router, prefix="/v1")
+
+
+#  healthcheck
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
